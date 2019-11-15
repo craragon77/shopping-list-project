ea45058e1742fd5d481f5204cda3c655d51bb852
Chris Aragon <aragonchristopherray@gmail.com>
2019-11-15 13:15:26 -0500

    first commit

---
+++ shopping-list.js
@@ -0,0 +1,14 @@
+//function that makes the item that is typed into the form, makes the thing
+$("js-shopping-list-form").on('click', 'li', function(){
+    event.preventDefaultBehaivor;
+    this.append("<li>")
+});
+
+
+
+
+//function that makes the thing disappear when the dissapear button is clicked
+
+
+
+//
