63b2e15098f8f2650e5cf811e4cab2f938815ec0
Chris Aragon <aragonchristopherray@gmail.com>
2019-11-18 17:44:37 -0500

    pushing the js

--- shopping-list.js
+++ shopping-list.js
@@ -1,31 +1,31 @@
 //function to add the item to the list
-$(function() {
-$("#js-shopping-list-form").on('click', 'li', function(event){
-    event.preventDefaultBehavior();
-    $("shopping-list").append("<li>")
-});
+function list() {
 
-
-
-
-
-
+const buttonClass = $(".js-shopping-list-form");
+buttonClass.find("button").click(function(event) {
+    event.preventDefault();
+    $(".shopping-item").append().text();
+    });
+    
+    
+    
+    
+    
+    
 //function that makes the thing disappear when the dissapear button is clicked
-
-
-
-const list = $(".shopping-list")
+    
+    
+    
+const list = $(".shopping-list");
     $(".shopping-item-toggle").click(function(event) {
-        event.currentTarget();
-    list.find(".shopping-item").toggle("shopping-item__checked");
-});
-
-
-
-//function that deletes the item
+    this.addClass(shopping-item__checked);
+    });
+    
+    
+    //function that deletes the item
 $("shopping-list-delete").click(function(event) {
     event.stopPropagation();
-    list.find(".shopping-item").remote()
+    list.find(".shopping-item").remote();
 })
-
-});
+    
+}
