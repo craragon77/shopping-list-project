a585bd9b32cced48dade03e27e3fbc5e909cf009
Chris Aragon <aragonchristopherray@gmail.com>
2019-11-19 20:23:03 -0500

    you got the first one

--- shopping-list.js
+++ shopping-list.js
@@ -3,7 +3,19 @@
 
 $("#js-shopping-list-form").find("button").click(function(event) {
     event.preventDefault();
-    $(".shopping-list").append("<li>",$("#shopping-list-entry"))
+    $(".shopping-list").append(
+        `<li>
+        <span class="shopping-item">${$('#shopping-list-entry').val()}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`
+    )
 })
     
     
